458c4fc5591ad14aab0e92d111a75fdca92d91d1
Suhail vs <suhail@capsone.com>
2013-09-18 17:09:26 +0530

    changed render_to_rensponse to render and arranged in folder

---
+++ quiz/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from quiz.models import Quiz, Score
+from quiz.forms import quiz_forms, QuestionForm
+
+def quiz_dashboard(request):
+	student = request.user
+	quizzes = Quiz.objects.filter(students=student)
+	scores = Score.objects.filter(student=student)
+	
+	return render(request,'quiz/quiz_dashboard.html', { 'quizzes': quizzes, 'scores': scores })
+
+def quiz_detail(request, slug):
+	quiz = get_object_or_404(Quiz, slug__iexact=slug)
+	try:
+		score = quiz.score_set.get(student=request.user, quiz=quiz)
+		questions = quiz.question_set.all()
+		corrent_anwser = score.corrent_anwsers
+		return render(request,'quiz/quiz_score.html', { 'quiz': quiz, 'score': score, 'questions': questions, 'corrent_anwser': corrent_anwser })
+	except Score.DoesNotExist:
+		form_list = quiz_forms(quiz)
+		return render(request,'quiz/quiz_detail.html', { 'form_list': form_list, 'quiz': quiz }, context_instance=RequestContext(request))
+
+def process_quiz(request, slug):
+	quiz = get_object_or_404(Quiz, slug__iexact=slug)
+	
+	if request.method == 'POST':
+		form_list = quiz_forms(quiz, request.POST)
+		score = Score()
+		score.student = request.user
+		score.quiz = quiz
+		score.save()
+		
+		for form in form_list:
+			if form.is_correct():
+				score.corrent_anwsers = form.question
+				score.save()
+		
+		return HttpResponseRedirect(score.get_absolute_url())
+	
+	return HttpResponseRedirect(quiz.get_absolute_url())
